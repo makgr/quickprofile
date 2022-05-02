b3d256cf1ed1390e8f5c8ef703c5f14a853281b1
Mohammad Ali Khan <xvirus.bd@gmail.com>
2022-05-02 22:22:42 +0600

    migration for registers done

--- database/migrations/2022_05_02_155710_create_registers_table.php
+++ database/migrations/2022_05_02_155710_create_registers_table.php
@@ -15,6 +15,12 @@ class CreateRegistersTable extends Migration
     {
         Schema::create('registers', function (Blueprint $table) {
             $table->id();
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('user_name')->unique();
+            $table->integer('mobile');
+            $table->string('about_me',500)->nullable();
+            $table->string('password');
             $table->timestamps();
         });
     }
